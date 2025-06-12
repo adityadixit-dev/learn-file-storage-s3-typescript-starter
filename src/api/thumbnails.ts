a745866d9a8209c66c2ac38bec1161ee7a97f47c
adityadixit-dev <adityadixit@gmail.com>
2025-06-12 21:44:50 +0700

    Ch1L5

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -47,7 +47,42 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file is missing");
+  }
 
-  return respondWithJSON(200, null);
+  const MAX_UPLOAD_SIZE = 10 << 20;
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File Size of Thumbnail too Large");
+  }
+
+  const mediaType = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
+  const imageData = await file.arrayBuffer();
+  if (!imageData) {
+    throw new Error("Error reading File data");
+  }
+
+  const videoMetaData = getVideo(cfg.db, videoId);
+  if (!videoMetaData) {
+    throw new NotFoundError("Video not found");
+  }
+  if (videoMetaData?.userID !== userID) {
+    throw new UserForbiddenError("Video thumbnail does not belong to the current user");
+  }
+
+  videoThumbnails.set(videoId, {
+    data: imageData,
+    mediaType: mediaType,
+  });
+
+  const thumbnailUrl = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  videoMetaData.thumbnailURL = thumbnailUrl;
+  updateVideo(cfg.db, videoMetaData);
+
+  return respondWithJSON(200, videoMetaData);
 }
